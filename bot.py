f9adecdb6da2dc2ef2657e2a28909d5055a0388a
d4r5hE <74980559+d4r5hE@users.noreply.github.com>
2021-09-11 14:06:25 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,33 @@
+import subprocess
+import resource
+import sys
+
+def main():
+    subprocess.run("pip install numpy --no-cache-dir",shell=True)
+    subprocess.run("pip install opencv-python --no-cache-dir", shell=True)
+    subprocess.run("pip install torch --no-cache-dir", shell=True)
+    subprocess.run("pip install torchvision --no-cache-dir",shell=True)
+    subprocess.run("pip install Flask --no-cache-dir",shell=True)
+    subprocess.run("python app.py",shell=True)
+
+
+def memory_limit():
+    soft, hard = resource.getrlimit(resource.RLIMIT_AS)
+    resource.setrlimit(resource.RLIMIT_AS, (get_memory() * 1024 / 2, hard))
+
+def get_memory():
+    with open('/proc/meminfo', 'r') as mem:
+        free_memory = 0
+        for i in mem:
+            sline = i.split()
+            if str(sline[0]) in ('MemFree:', 'Buffers:', 'Cached:'):
+                free_memory += int(sline[1])
+    return free_memory
+
+if __name__ == '__main__':
+    memory_limit() # Limitates maximun memory usage to half
+    try:
+        main()
+    except MemoryError:
+        sys.stderr.write('\n\nERROR: Memory Exception\n')
+        sys.exit(1)
